f1fc11514c0cdb0cc187a20843258b9885c0c74a
shanellobo <shanelllobo06@gmail.com>
2026-01-30 12:12:02 +0530

    Delete consecutiveint.py

--- consecutiveint.py
+++
@@ -1,6 +0,0 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-for i in range(min(a, b), 0, -1):
-    if a % i == 0 and b % i == 0:
-        print("GCD is:", i)
-        break
