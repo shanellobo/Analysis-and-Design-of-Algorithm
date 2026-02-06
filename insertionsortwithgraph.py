53c01dc8978ac3f33a0a5ccc0f073e9d13e3ff66
shanellobo <shanelllobo06@gmail.com>
2026-02-06 12:39:45 +0530

    Add files via upload

---
+++ insertionsortwithgraph.py
@@ -0,0 +1,38 @@
+import random
+import time
+import matplotlib.pyplot as plt
+
+def insertionsort(arr):
+    n=len(arr)
+    for i in range(1,n):
+        j=i-1
+        v=l[i]
+        while j>=0 and arr[j]>v:
+            arr[j+1]=l[j]
+            j=j-1
+        arr[j+1]=v
+            
+n_list = [5000, 6000, 7000, 8000, 9000, 10000]
+sort_time = []
+
+for n in n_list:
+    l = [random.randint(1, 100) for _ in range(n)]
+    s_t = time.time()
+    insertionsort(l)
+    e_t = time.time()
+
+    sort_time.append(e_t - s_t)
+
+print("Input sizes:", n_list)
+print("Sorting times:", sort_time)
+
+
+#plotting the graph
+plt.plot(n_list,sort_time, marker='.')
+plt.xlabel("Number of Elements (n)")
+plt.ylabel("Time Taken (seconds)")
+plt.title("insertion sort: Time Complexity Analysis")
+plt.grid(False)
+#save the plot
+# plt.savefig("selection_sort_time_complexity.png", dpi=300, bbox_inches='tight')
+plt.show()
